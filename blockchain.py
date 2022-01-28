8116fd9639f79224344d46cae9fa943c1f4a577a
Mark Churcher <mark@developersinstitute.ac.nz>
2022-01-28 16:09:30 +1300

    prompt user for transaction amount

--- blockchain.py
+++ blockchain.py
@@ -11,10 +11,7 @@ def get_last_blockchain_value():
 def add_value(transaction_amount, last_transaction=get_last_blockchain_value()):
     blockchain.append([last_transaction, transaction_amount])
 
-
-add_value(1)
-add_value(2)
-add_value(3)
-add_value(6)
+tx_amount = input('enter amount:');
+add_value(float(tx_amount))
 
 print(blockchain)
